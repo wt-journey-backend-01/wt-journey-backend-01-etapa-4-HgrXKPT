057b905d872fa6bcab4c027bf328e86bd14b2c9e
Higor <higorestevao02@hotmail.com>
2025-08-23 00:57:54 -0300

    Fix: Seeds corrigidas para rodar  oserver

--- db/seeds/usuarios.js
+++ db/seeds/usuarios.js
@@ -8,7 +8,7 @@ exports.seed = async function(knex) {
   await knex('usuarios').insert([
 
     {nome: "Pedro", email: "pedro@gmail.com", senha: "$2b$10$C1o1d/MeMmcUJerJEp1Cr.0omC/M1W5gY7MuetkBPA6U4bxgVokBG"},
-    {nome: "Ruan", email: "ruan@gmail.com", senha: "hashed_password_2"}
+    {nome: "Ruan", email: "ruan@gmail.com", senha: "$2b$10$6rwx8ri11j9YXaStDz0gCOP.u3jsXkqH4CV7UyFs6ayOnLfmOPozO"}
   
     
   ]);
