73da0f69ce7598117f5b3a477ee8166622f835e9
Higor <higorestevao02@hotmail.com>
2025-08-23 13:18:39 -0300

    Fix: Tentando voltar para os 72 pontos

--- controllers/authController.js
+++ controllers/authController.js
@@ -45,11 +45,11 @@ async function login(req, res){
        return res.status(401).json({ message: "Senha inválida" });
     }
 
-    const acessToken = tokenUtils.generateAccessToken(user);
+    const accessToken = tokenUtils.generateAccessToken(user);
     const refreshToken = tokenUtils.generateRefreshToken(user);
 
     return res.status(200).json({
-        acess_token: acessToken,
+        access_token: accessToken,
         refresh_token: refreshToken
         });
 }
